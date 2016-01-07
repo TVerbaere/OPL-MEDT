9956dafda9c8c5038eb38cec961431e1d03a76b3
thibaud <thibaud.verbaere@gmail.com>
2016-01-07 10:31:09 +0100

    init project

---
+++ MagicalExperimentalDebugTool/src/main/java/com/iagl/opl/MagicalExperimentalDebugTool/App.java
@@ -0,0 +1,13 @@
+package com.iagl.opl.MagicalExperimentalDebugTool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
